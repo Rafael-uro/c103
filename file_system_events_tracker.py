241521a6caee431863eef2c68e52972135d9df62
Rafael-uro <94479855+Rafael-uro@users.noreply.github.com>
2023-04-21 00:01:19 -0300

    Add files via upload

---
+++ file_system_events_tracker.py
@@ -0,0 +1,36 @@
+import sys
+import time
+import random
+import os
+import shutil
+
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = r"C:/Users/Suiene.uró/Downloads"
+
+class FileEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"olá, {event.src_path} foi criado!")
+    def on_deleted(self, event):
+        print(f"Opa! Alguém excluiu {event.src_path}!")
+
+event_handler = FileEventHandler();
+
+
+observer = Observer();
+
+
+observer.schedule(event_handler, from_dir, recursive = True);
+
+
+observer.start();
+
+try:
+     while True:
+         time.sleep(2)
+         print("executando...")
+except KeyboardInterrupt:
+    print("interrompidol")
+    observer.stop()
